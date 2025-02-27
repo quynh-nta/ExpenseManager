65b4511ff371cc856f5629e9ddce139bc3f16fc2
quynh-nta <38488993+nguyenthianhquynh@users.noreply.github.com>
2025-02-27 09:24:40 +0700

    Initial Commit

---
+++ ExpenseManager/ExpenseManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseManagerApp.swift
+//  ExpenseManager
+//
+//  Created by Nguyen Thi Anh Quynh on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
